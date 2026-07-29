0edd4e65d235482f098de4d8e4e9edf7c0221008
arutyunyanalik2-a11y <arutyunyanalik2@xmail.ru>
2026-07-29 12:23:48 +0400

    Add files via upload

--- models/Store.js
+++ models/Store.js
@@ -5,7 +5,8 @@ const storeSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     address: { type: String, required: true },
-    zone: { type: String, required: true }
+    zone: { type: String, required: true },
+    discountPercent: { type: Number, default: 0 } // 👈 Добавлено поле для автоматической скидки
 });
 
 module.exports = mongoose.model('Store', storeSchema);
